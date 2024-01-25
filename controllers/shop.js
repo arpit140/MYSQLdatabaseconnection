a64a8ef650cf474d0822c5c34a7d9b987fcf77d7
arpit140 <arpitsunny6@gmail.com>
2024-01-25 17:55:40 +0530

    database

--- controllers/shop.js
+++ controllers/shop.js
@@ -22,15 +22,18 @@ exports.getProduct = (req, res, next) => {
   });
 };
 
-exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
-    });
-  });
-};
+// exports.getIndex = (req, res, next) => {
+//   Product.fetchAll()
+//       .then(([rows,fieldDAta]) => {    
+//         res.render('shop/index', {
+//         prods: rows,
+//         pageTitle: 'Shop',
+//         path: '/'
+//     });
+//   })
+//       .catch(err => {console.log(err)})
+
+// };
 
 exports.getCart = (req, res, next) => {
   Cart.getCart(cart => {
